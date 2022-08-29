1e679a39a4cb7ef77be0b9b0702137ed3eaec690
Sheikh sadi <you@example.com>
2022-08-29 15:10:00 -0700

    todo added

---
+++ todo.js
@@ -0,0 +1,21 @@
+function todoList (){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(data => displayTodo(data))
+}
+
+function displayTodo(todos){
+    const todoContainer = document.getElementById('todo-container');
+    for(const todo of todos){
+        console.log(todo);
+        const todoDiv = document.createElement('todoDiv');
+        todoDiv.innerHTML = `
+        <h2>userId: ${todo.id}</h2>
+        <h4>title: ${todo.title}</h4git add .>
+        <p>Is completed: ${todo.completed === true ? 'completed' : 'not completed'}</p>
+        `;
+        todoContainer.appendChild(todoDiv);
+    }
+}
+
+todoList();
