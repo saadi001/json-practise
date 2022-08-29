d20a757b49c41becd1e69366ac28807a64719ffd
Sheikh sadi <you@example.com>
2022-08-29 05:04:54 -0700

    post dynamically added

---
+++ post.js
@@ -0,0 +1,23 @@
+function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+   .then(res => res.json())
+   .then(data => displyPost(data))
+
+}
+
+function displyPost(data){
+    const postContainer = document.getElementById('post-container');
+    for(const detail of data){
+        // console.log(detail);
+        const div = document.createElement('post-container');
+        div.classList.add('stl');
+        div.innerHTML = `
+        <h3>User-${detail.id}</h3>
+        <h4>post: ${detail.title}</h4>
+        <p>description: ${detail.body}</p>
+        `;
+        postContainer.appendChild(div);
+    }
+}
+
+loadPost();
