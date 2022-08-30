9a6027d9607f06cfcc3f16c3d8fda0c5b56e7314
Sheikh sadi <you@example.com>
2022-08-30 06:12:22 -0700

    countried function added

--- countries.js
+++ countries.js
@@ -10,15 +10,37 @@ const displayCountries = countries =>{
         
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country')
-        console.log(country);
+        // console.log(country);
         countryDiv.innerHTML = `
             <h3>Name: ${country.name.common}</h3>
             <h5>Capital: ${country.capital ? country.capital[0] : 'No capital'}</h5>
             <p>population: ${country.population}</P>
+            <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
         `;
         countryContainer.appendChild(countryDiv);
 
     })
 }
+
+const loadCountryDetail = (code) =>{
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data))
+
+}
+
+const displayCountryDetails = (country) =>{
+    console.log(country);
+    const detailContainer = document.getElementById('details-country');
+    detailContainer.innerHTML = `
+    <h2>Name: ${country.name.common}</h2>
+
+    `;
+
+
+}
+
+
 
 loadCoutries();
