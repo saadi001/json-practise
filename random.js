35effc2af04fb51d056b7a68ffd79ccf62c6f45c
Sheikh sadi <you@example.com>
2022-08-30 02:57:50 -0700

    random users adde

---
+++ random.js
@@ -0,0 +1,24 @@
+const loadRandom = () => {
+    fetch('https://randomuser.me/api/?results=100')
+    .then(res => res.json())
+    .then(data => displayRandom(data.results))
+}
+
+const displayRandom = users =>{
+    const userContainer = document.getElementById('user-container');
+    for(const user of users){
+        console.log(user);
+        const userDiv = document.createElement('div');
+        userDiv.classList.add('user-style')
+        userDiv.innerHTML = `
+            <h3>Name: ${user.name.first} ${user.name.last}</h3>
+            <h5>email: ${user.email} </h5>
+            <h5>Gender: ${user.gender}</h5>
+            <p> location: ${user.location.city}, ${user.location.country}</p>
+            
+        `;
+        userContainer.appendChild(userDiv);
+    }
+}
+
+loadRandom();
