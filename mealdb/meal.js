27e4854c11be000bf0c96170e414fccba7cfa5ba
Sheikh sadi <you@example.com>
2022-08-30 14:40:52 -0700

    div onclick added

--- mealdb/meal.js
+++ mealdb/meal.js
@@ -7,12 +7,13 @@ const loadMeals = (search) =>{
 
 const displayMeals = meals => {
     const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = ``;
     meals.forEach(meal =>{
-        console.log(meal);
+        // console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">${meal.strMeal}</h5>
@@ -25,4 +26,35 @@ const displayMeals = meals => {
   
 }
 
-loadMeals('pizza');
+const searchFood = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadMeals(searchText);
+    searchField.value = "";
+}
+
+const loadMealDetail = (idMeal) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = meal =>{
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = ``;
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    detailContainer.appendChild(mealDiv);
+}
+
+loadMeals('a');
