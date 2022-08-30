f0b9fcc2fced67a8dea675b0019d242b44acc800
Sheikh sadi <you@example.com>
2022-08-30 12:33:46 -0700

    bootstrap dynamically added

--- mealdb/meal.js
+++ mealdb/meal.js
@@ -1,5 +1,26 @@
 const loadMeals = () =>{
-    fetch('www.themealdb.com/api/json/v1/1/search.php?f=a')
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayMeals(data.meals))
 }
+
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meal-container');
+    meals.forEach(meal =>{
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+        <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+        `;
+        mealContainer.appendChild(mealDiv);
+    })
+  
+}
+
+loadMeals();
