7b45eab817b4101775a55535c5ec045db20eacb4
Sheikh sadi <you@example.com>
2022-08-30 13:03:47 -0700

    search by function added

--- mealdb/meal.js
+++ mealdb/meal.js
@@ -1,5 +1,6 @@
-const loadMeals = () =>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+const loadMeals = (search) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals))
 }
@@ -7,13 +8,14 @@ const loadMeals = () =>{
 const displayMeals = meals => {
     const mealContainer = document.getElementById('meal-container');
     meals.forEach(meal =>{
+        console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
         <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
+                  <h5 class="card-title">${meal.strMeal}</h5>
                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
               </div>
@@ -23,4 +25,4 @@ const displayMeals = meals => {
   
 }
 
-loadMeals();
+loadMeals('pizza');
